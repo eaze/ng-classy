4beb13265957e578065c04a10a549c4da07a036d
Nik Butenko <nik@butenko.me>
2015-06-18 23:20:52 +1000

    Coverage report #2

--- karma.conf.js
+++ karma.conf.js
@@ -1,28 +1,73 @@
+
+var path = require('path');
+var webpack = require('webpack');
+
+var webpackConfig = {
+  devtool: 'inline-source-map',
+
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel?stage=0',
+        include: path.join(__dirname, 'test')
+      },
+      {
+        test: /\.js$/,
+        loader: 'isparta',
+        include: path.join(__dirname, 'lib')
+      }
+    ],
+    loaders: [
+    ]
+  },
+  plugins: [
+  ],
+  stats: {
+    colors: true
+  }
+};
+
+
 module.exports = function(config) {
   config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-    frameworks: ['mocha', 'browserify'],
+    frameworks: ['mocha'],
     files: [
       'node_modules/angular/angular.js',
       'node_modules/angular-ui-router/release/angular-ui-router.js',
       'node_modules/angular-mocks/angular-mocks.js',
-      'test/**/*.js',
+      'test/**/*.js'
     ],
     exclude: [
     ],
+    webpack: webpackConfig,
+    webpackMiddleware: {
+      quiet: true
+    },
     preprocessors: {
-      'test/**/*.js': ['browserify'],
+      'test/**/*.js': ['webpack']
     },
-    browserify: {
-      debug: true,
-      transform: [ ['babelify', {sourceMap: 'inline', stage: 0}] ],
-      extensions: ['.js'],
-    },
+
     browsers: [
       'Chrome'
     ],
     reporters: ['progress'],
+    coverageReporter: {
+      dir: './coverage/',
+      subdir: function (browser) {
+        return browser.toLowerCase().split(/[ /-]/)[0];
+      },
+      reporters: [
+        {type: 'text', file: 'text.txt'},
+        {type: 'text-summary', file: 'text-summary.txt'},
+        {type: 'html'}
+      ]
+    },
     port: 9876,
     colors: true,
     logLevel: 'INFO',
